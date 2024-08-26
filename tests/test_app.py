24360221353138d12136d28ca0d16e5424e31e43
Jofralso <fsoares@estgl.ipv.pt>
2024-08-26 02:37:10 +0100

    updated

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,11 @@
-import pytest
+import sys
+import os
+
+# Add the root directory to sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import app
+import pytest
+
 
 @pytest.fixture
 def client():
